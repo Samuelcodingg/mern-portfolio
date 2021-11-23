54d6ccca7609a7e7f481cbfa159fc84351f92e07
Samuelcodingg <samuelroman39@gmail.com>
2021-11-23 14:45:28 -0500

    main section is completed

--- frontend/src/components/homepage/HomePage.js
+++ frontend/src/components/homepage/HomePage.js
@@ -3,17 +3,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { getCoder } from '../../api/coder';
+import { API } from '../../config';
 import imgCoding from './undraw_freelancer_re_irh4.svg';
 
 export const HomePage = () => {
 
     const [coder, setCoder] = useState([]);
 
-    const { name, title, first_description, second_description } = coder;
+    const { _id ,name, title, first_description, second_description } = coder;
 
     const loadDataCoder = async () => {
         const data = await getCoder();
         setCoder({
+            _id: data[0]._id,
             name: data[0].name,
             title: data[0].title,
             first_description: data[0].first_description,
@@ -37,15 +39,39 @@ export const HomePage = () => {
                     <div className="row text-center d-flex align-items-center">
                         <Link
                             to="/about"
-                            className="bg-primary-color text-white text-decoration-none d-block col-5 rounded py-2 mx-auto"
+                            className="bg-primary-color text-white text-decoration-none d-block col-5 rounded fw-bold hover-bg-dark animation-time py-2 mx-auto"
                         >
                             VER PERFIL  <FontAwesomeIcon icon={faUser} />
                         </Link>
                         <Link 
                             to="/projects" 
-                            className="col-5 offset-1 mx-auto text-decoration-none d-block border-primary-color primary-color py-2" 
+                            className="col-5 offset-1 mx-auto text-decoration-none d-block border-primary-color fw-bold hover-bg-dark animation-time primary-color py-2" 
                         >
                             VER PROYECTOS <FontAwesomeIcon icon={faFolder} />
+                        </Link>
+                    </div>
+                    <div className="shadow-lg px-4 py-3 mt-3">
+                        <div className="d-flex justify-content-between align-items-center">
+                            <div className="d-flex align-items-center">
+                                <div>
+                                    <img src={`${API}/coders/photo/${_id}`} alt="samuel-roman" className="rounded-circle img-fluid" style={{height: '50px', width: '50px'}} />
+                                </div> 
+                                <div className="ms-2">
+                                    <h5 className="mb-0">{name}</h5>
+                                    <p className="text-secondary mb-0">{title}</p>
+                                </div>
+                            </div>
+                            <Link className="text-secondary text-decoration-none hover-bg-dark animation-time">
+                                ENVÍAME UN MENSAJE &nbsp;
+                                <i className="fas fa-envelope"></i>
+                            </Link>
+                        </div>
+                        <Link
+                            to="/cv"
+                            className="mt-3 text-center primary-color text-decoration-none fw-bold d-block w-100 hover-bg-dark animation-time"
+                        >
+                            VER CURRICULUM &nbsp;
+                            <i className="fas fa-file-alt"></i>
                         </Link>
                     </div>
                 </div>
--- frontend/src/components/ui/NavbarComponent.js
+++ frontend/src/components/ui/NavbarComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faFolder, faQuestionCircle, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faFolder, faQuestionCircle, faUser } from '@fortawesome/free-solid-svg-icons'
 
 export const NavbarComponent = () => {
     return (
